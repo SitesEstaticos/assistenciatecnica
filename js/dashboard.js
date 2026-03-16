6608986ac2115877c017b14fc9fc1fccc73a375b
Matias M. Martins <mathyasmartins25@gmail.com>
2026-03-16 17:16:19 -0300

    cor vermelha grafico em manutenção

--- js/dashboard.js
+++ js/dashboard.js
@@ -137,7 +137,7 @@ async function initCharts() {
                             '#dbeafe',
                             '#fef3c7',
                             '#fecaca',
-                            '#86ac7a',
+                            '#cd9ab0',
                             '#dcfce7',
                             '#d1fae5'
                         ],
